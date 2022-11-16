db74bae8fb3693673eaf2eeda341fca50a5a88fd
Thomas <Raithdk@gmail.com>
2022-11-16 22:00:05 +0100

    adding api calls to backend

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
@@ -9,5 +13,5 @@ def hello_world():
 @app.route('/post', methods=["POST"])
 def postTest():
     input_json = request.get_json(force=True)
-    dictToReturn = {'text':input_json['text']}
+    dictToReturn = {'text':input_json['url']}
     return jsonify(dictToReturn)
--- react-frontend/src/app/components/Recipes.js
+++ react-frontend/src/app/components/Recipes.js
@@ -5,15 +5,38 @@ import {Button, Form, InputGroup } from "react-bootstrap";
 function Recipes(){
     const [recipeUrl, setRecipeUrl] = useState("");
 
+    function requestRecipe() {
+        fetch('http://localhost:5000/post',{ 
+            method: 'Post',
+            body: JSON.stringify({
+                'url': {recipeUrl}
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },})
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                //handle request return
+            })
+            .catch((err) => {
+                console.log(err.message)
+                //handle err
+            });
+        
+    }
+
     return(
         <div>
             <h1>Recipes!</h1>
             <div className="mx-5">
                 <InputGroup >
                     <Form.Control
-                    placeholder={recipeUrl}/>
-                    <Button>Add new Recipe</Button>
-                    
+                    placeholder={recipeUrl}
+                    onChange={(e) => setRecipeUrl(e.target.value)}/>
+                    <Button
+                    onClick={() => requestRecipe()}
+                    >Add new Recipe</Button>
                 </InputGroup>
             </div>
         </div>
